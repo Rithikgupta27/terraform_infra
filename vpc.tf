a15cab4bc6d6e6927bfd97a1e759ae059ad96305
Rithikgupta27 <rithikg2700@gmail.com>
2024-09-18 01:20:35 +0530

    vpc subnet

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,25 @@
-resource "aws_vpc" "dev_vpc" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
 tags = {
   name = "dev_vpc"
 }
-
 }
 
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "subnet-2"
+  }
+}
