40f131a4da140ad0091b2db42351e5131ed7a79f
Rithikgupta27 <rithikg2700@gmail.com>
2025-05-26 00:26:30 +0530

    Created: route table and routes

--- Infra-1/vpc.tf
+++ Infra-1/vpc.tf
@@ -43,3 +43,72 @@ resource "aws_subnet" "private-subnet-2" {
   }
 }
 
+resource "aws_internet_gateway" "nginx-igw" {
+  vpc_id = aws_vpc.nginx-demo.id
+  tags = {
+    Name = "nginx-igw"
+  }
+
+}
+
+resource "aws_eip" "nginx-eip" {
+   vpc = true
+}
+
+resource "aws_nat_gateway" "nginx-ngw" {
+  allocation_id = aws_eip.nginx-eip.id
+  subnet_id = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name = "nginx-ngw"
+  }
+  
+}
+
+resource "aws_route_table" "nginx-public-route-tb" {
+  vpc_id = aws_vpc.nginx-demo.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nginx-igw.id
+  }
+
+  tags = {
+    Name = nginx-public-route-tb
+  }
+
+}
+
+resource "aws_route_table" "nginx-private-route-tb" {
+  vpc_id = aws_vpc.nginx-demo.id
+
+  route = {
+    cidr_block = "10.0.0.0/16"
+    gateway_id =  aws_internet_gateway.nginx-igw.id
+  }
+
+  tags = {
+    Name = "nginx-private-route-tb"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet-association-1" {
+  subnet_id = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.nginx-public-route-tb.id
+}
+
+resource "aws_route_table_association" "public-subnet-association-2" {
+  subnet_id = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.nginx-public-route-tb.id
+}
+
+resource "aws_route_table_association" "private-subnet-association-1" {
+  subnet_id = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.nginx-private-route-tb.id
+}
+
+resource "aws_route_table_association" "private-subnet-association-2" {
+   subnet_id = aws_subnet.private-subnet-2.id
+   route_table_id = aws_route_table.nginx-private-route-tb.id
+}
+}
